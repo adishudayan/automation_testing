0002e8931ee6bae664620801aee46193cfe4c081
adishudayan <adish.udayan@ayatacommerce.com>
2022-07-21 18:23:08 +0530

    added return text and click functionality test

--- test/element.js
+++ test/element.js
@@ -1,12 +1,29 @@
 describe("interaction with web elements",function(){
 
-        it("enter value in a field", function(){
-            browser.url('/');
+        it("enter value in a field", async function(){
+            browser.url('https://www.amazon.com/');
             const search = $('#twotabsearchtextbox');
-            search.setValue('Apple Mac Book');
+            await search.setValue('Apple Mac Book');
+            browser.pause(5000);
+
+            // const label = $('span.a-size-base.a-color-base');
+            // let text = label.getText();
+            // console.log(text);
             
-        
+
         });
+        it("get text of field",async function(){
+            browser.url('https://www.amazon.com/');
+            const label =await $('span.a-size-base.a-color-base');
+            let text = await label.getText();
+            await console.log(text);
+        });
+
+        it("click on a field",async function(){
+            browser.url('https://www.amazon.com/');
+            const icon =await $('input.nav-input');
+            await icon.click();
+        })
         
 
 });
